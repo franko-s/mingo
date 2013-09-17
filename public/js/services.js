3793e8b627c41c92588658224161936192bba0ef
Franko <viveyssozi@gmail.com>
2013-09-16 20:53:51 -0400

    dranko

--- public/js/services.js
+++ public/js/services.js
@@ -245,7 +245,7 @@ factory('FacebookSvr', [function() {
             this.authenticated().done(fn);
     },
 	this.getMutualFriendsCount = function(uid, callback){
-		FB.api(encodeURIComponent('/fql?select mutual_friend_count from user where uid = '+uid), function (response) {
+		FB.api(encodeURIComponent("/fql?select mutual_friend_count from user where uid='"+uid+"'"), function (response) {
 				//alert(response);
 			console.log(response);
 			callback(response.mutual_friend_count);
